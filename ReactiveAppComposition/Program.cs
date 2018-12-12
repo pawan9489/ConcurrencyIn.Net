f60c4d9712aaa54c05130cbe08989f625dcf14ce
pawan9489 <pawankumar.gubbala@gmail.com>
2018-12-12 10:40:45 +0530

    Rx

---
+++ ReactiveAppComposition/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reactive.Linq;
+
+namespace ReactiveAppComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // To test IObservables => create Virtual event stream by converting IEnumerable to IObservable
+            var textBox = new TextBox();
+            var form = new Form
+            {
+                Controls = { textBox }
+            };
+
+            var msg = Observable.FromEventPattern(textBox, "TextChanged");
+            var text = (from e in msg
+                       select ((TextBox)e.Sender).Text)
+                       .DistinctUntilChanged()
+                       .Throttle(TimeSpan.FromSeconds(0.5));
+
+            Console.WriteLine(getN(new Person()));
+            using (text.Subscribe(Console.WriteLine))
+            {
+                Application.Run(form);
+            }
+        }
+
+        static int getN(Person p)
+        {
+            return p?.Name?.Substring(1, 2).Length ?? -1;
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
